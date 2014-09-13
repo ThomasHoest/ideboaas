bcafc1035a224393a079a8dcfd838a5862cb98fe
Thomas-Telerik <thomas.andersen@telerik.com>
2014-09-13 20:15:38 +0200

    able to register

--- IdeboaasWeb/Migrations/Configuration.cs
+++ IdeboaasWeb/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace IdeboaasWeb.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(IdeboaasWeb.Models.ApplicationDbContext context)
