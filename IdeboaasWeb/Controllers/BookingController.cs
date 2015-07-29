350712d09403bf40ce2b4d02a826e2a8a9bf38a9
Thomas-Telerik <zoglub@gmail>
2015-07-29 11:49:25 +0200

    Bookings can be saved

--- IdeboaasWeb/Controllers/BookingController.cs
+++ IdeboaasWeb/Controllers/BookingController.cs
@@ -1,52 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
 
 namespace IdeboaasWeb.Controllers
 {
-  public class BookingController : Controller
-  {
-    public ActionResult Index()
+    public class BookingController : Controller
     {
-      return View(ReadBookings());
-    }
-
-    //
-    // POST: /Account/Book
-    [HttpPost]
-    public void Book(Booking booking)
-    {
-      
-    }
-
-    private CalenderContents ReadBookings()
-    {
-      CalenderContents cc = new CalenderContents();
-      cc.Bookings = new Booking[]
-      {
-        new Booking()
+        public ActionResult Index()
         {
-          title = "Test",color = "blue",start = DateTime.Now.ToString("yyyy-M-d"), end =  DateTime.Now.AddDays(7).ToString("yyyy-M-d")
+            return View(ReadBookings());
         }
-      };
-      return cc;
+
+        //
+        // POST: /Account/Book
+        [HttpPost]
+        public void Book(Booking booking)
+        {
+            if (booking != null)
+            {
+                CalenderContents cc = ReadBookings();
+                var bookings = new List<Booking>(cc.Bookings);
+                bookings.Add(booking);
+                cc.Bookings = bookings.ToArray();
+                SaveBookings(cc);
+            }
+        }
+
+        private CalenderContents ReadBookings()
+        {
+            string path = HttpContext.Server.MapPath("~/App_Data/bookings.json");
+            string json;
+            using (StreamReader sr = new StreamReader(new FileStream(path, FileMode.Open)))
+            {
+                json = sr.ReadToEnd();
+            }
+
+            CalenderContents cc = new CalenderContents();
+
+            if (!string.IsNullOrEmpty(json))
+            {
+                cc.Bookings = JsonConvert.DeserializeObject<Booking[]>(json);
+            }
+            return cc;
+        }
+
+        private void SaveBookings(CalenderContents cc)
+        {
+            string path = HttpContext.Server.MapPath("~/App_Data/bookings.json");
+            string json;
+            using (StreamWriter sw = new StreamWriter(new FileStream(path, FileMode.Open)))
+            {
+                sw.WriteLine(JsonConvert.SerializeObject(cc.Bookings));
+            }
+        }
     }
-  }
 
-  public class CalenderContents
-  {
-    public Booking  [] Bookings{ get; set; }
-  }
+    public class CalenderContents
+    {
+        public Booking[] Bookings { get; set; }
+    }
 
-  public class Booking
-  {
-    public string title { get; set; }
-    public string start { get; set; }
-    public string end { get; set; }
-    public string color { get; set; }
-    public string owner { get; set; }
-  }  
+    public class Booking
+    {
+        public string title { get; set; }
+        public string start { get; set; }
+        public string end { get; set; }
+        public string color { get; set; }
+        public string owner { get; set; }
+    }
 }
--- IdeboaasWeb/Scripts/booking.js
+++ IdeboaasWeb/Scripts/booking.js
@@ -1,28 +1,45 @@
-/**
+﻿/**
  * Created by Thomas on 16-08-2014.
  */
 
-var page = new function(){  
-  this.initialize = function () {
-    var calender = $('#calendar-anchor').fullCalendar({
-      aspectRatio: 3,
-      events: bookings
-    });
-    
-    $('#save-booking').on('click',function() {
-      var booking = {
-        start: $('#booking-from').text(),
-        end: $('#booking-to').text(),
-        title: "booking",
-        color: 'blue'
-      };
+var page = new function () {
+    var page = this;
+    this._currentBooking = {};
 
-      $.post("Booking/book", booking, function() {
-        
-      });
-    });
+    this.selection = function (start, end) {
+        page._currentBooking = {
+            start: start.format("YYYY-MM-DD"),
+            end: end.format("YYYY-MM-DD")
+        }
 
-  }
+        $("#current-booking").first().text(start.format("DD-MM-YYYY") + " - " + end.format("DD-MM-YYYY"));
+    }
+
+    this.initialize = function () {
+        var calender = $('#calendar-anchor').fullCalendar({
+            aspectRatio: 3,
+            events: bookings,
+            selectable: true,
+            select: this.selection
+        });
+        var self = this;
+        $('#save-booking').on('click', function () {
+            if (self._currentBooking.start) {
+                var name = $("#booking-name")[0].value;
+                var type = $("#booking-type option:selected").text();
+                self._currentBooking.title = name;
+                self._currentBooking.color = type;
+                $.post("Booking/book", self._currentBooking, function() {
+
+                });
+            } else {
+                var $modal = $("#small-modal");
+                $modal.find("#small-modal-content").first().text('Do skal vælge en periode først');
+                $modal.modal("show");
+            }
+        });
+
+    }
 }
 
 
